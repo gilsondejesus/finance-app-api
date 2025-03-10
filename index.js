b741ea059d24a4d77f8fab45d53340e920169a72
Gilson de Jesus <gilsonmdj@gmail.com>
2025-03-10 16:25:48 -0300

    feat: add GetUserByEmailRepository

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import "dotenv/config.js";
 import express from "express";
 import { CreateUserController } from "./src/controllers/create-user.js";
-import { GetUserByIdController } from "./src/controllers/get-user-by-id.js";
+import { GetUserByIdController } from "./src/controllers/get-use-by-id.js";
 
 const app = express();
 
