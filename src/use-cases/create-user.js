d748a43afe28e166afc7cedb9372af99affd09e7
Gilson de Jesus <gilsonmdj@gmail.com>
2025-03-19 15:50:11 -0300

    feat: add UpdateUserUseCase

--- src/use-cases/create-user.js
+++ src/use-cases/create-user.js
@@ -7,7 +7,6 @@ import { EmailAlreadyInUseError } from "../errors/use.js";
 
 export class CreateUserUseCase {
   async execute(createUserParams) {
-    // TODO: verificar se o e-mail já está em uso
     const postgresGetUserByEmailRepository =
       new PostgresGetUserByEmailRepository();
 
