b8b70d86b5113364147e115cc0e46b70d6f5c21d
Gilson de Jesus <gilsonmdj@gmail.com>
2025-05-19 06:43:35 -0300

    feat: ensure Create and DeleteTransaction repositories use Prisma

--- src/repositories/postgres/transaction/create-transaction.js
+++ src/repositories/postgres/transaction/create-transaction.js
@@ -1,23 +1,8 @@
-import { PostgresHelper } from "../../../db/postgres/helper.js";
-
+import { prisma } from "../../../../prisma/prisma.js";
 export class PostgresCreateTransactionRepository {
   async execute(createTransactionsParams) {
-    const createdTransaction = await PostgresHelper.query(
-      `
-      INSERT INTO transactions (id, user_id, name, date, amount, type)
-       VALUES ($1, $2, $3, $4, $5, $6) 
-       RETURNING *
-      `,
-      [
-        createTransactionsParams.id,
-        createTransactionsParams.user_id,
-        createTransactionsParams.name,
-        createTransactionsParams.date,
-        createTransactionsParams.amount,
-        createTransactionsParams.type,
-      ],
-    );
-
-    return createdTransaction[0];
+    return await prisma.transaction.create({
+      data: createTransactionsParams,
+    });
   }
 }
