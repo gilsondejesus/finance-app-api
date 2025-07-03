0230e5deaf8bbf0fbe0983399ba5c3c3a0b5386a
Gilson de Jesus <gilsonmdj@gmail.com>
2025-07-03 06:57:19 -0300

    test: ensure 400 is returned if last_name is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -51,4 +51,25 @@ describe("Create User Controller", () => {
 
     expect(result.statusCode).toBe(400);
   });
+
+  it("should return 400 if last name is not provided", async () => {
+    // arrange
+    const createUserUseCase = new CreateUserUseCaseStub();
+    const createUserController = new CreateUserController(createUserUseCase);
+
+    const httpRequest = {
+      body: {
+        first_name: "Gilson",
+        email: "gilson@gmjcode.com",
+        password: "123456",
+      },
+    };
+
+    // act
+    const result = await createUserController.execute(httpRequest);
+
+    // assert
+
+    expect(result.statusCode).toBe(400);
+  });
 });
