9af95735fb8d7e8b8a079fc4246bfb2001eadeba
Gilson de Jesus <gilsonmdj@gmail.com>
2025-07-04 06:17:27 -0300

    test: ensure 400 is returned email is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -72,4 +72,25 @@ describe("Create User Controller", () => {
 
     expect(result.statusCode).toBe(400);
   });
+
+  it("should return 400 if email is not provided", async () => {
+    // arrange
+    const createUserUseCase = new CreateUserUseCaseStub();
+    const createUserController = new CreateUserController(createUserUseCase);
+
+    const httpRequest = {
+      body: {
+        first_name: "Gilson",
+        last_name: "De Jesus",
+        password: "123456",
+      },
+    };
+
+    // act
+    const result = await createUserController.execute(httpRequest);
+
+    // assert
+
+    expect(result.statusCode).toBe(400);
+  });
 });
